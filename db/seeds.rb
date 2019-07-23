cc90e35bdf7a7e14ede7a506973c71263813172f
steven ly <shiwenly@gmail.com>
2019-07-23 20:30:28 +0800

    ex 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new(
+    title: Faker::Quote.singular_siegler,
+    content: Faker::Quote.famous_last_words
+  )
+  article.save
+end
